21dace64755bafeaf7523fe5f5e937a34e1dc325
stalker57241 <stalker3204@yandex.com>
2022-12-31 19:17:54 +0300

    added commentaries

--- src/floatmath/floatops.c
+++ src/floatmath/floatops.c
@@ -3,12 +3,14 @@
 #include <stdlib.h>
 
 
+/** 
+ ** Adds FMFloat b to FMFloat a and writes at p_result
+*/
 unsigned int fmFloatOpAdd(FMFloat a, FMFloat b, FMFloat* p_result) {
     FMFloat a_mul;
     FMFloat b_mul;
 
     if (a.denominator != b.denominator) {
-        // printf("%d dwr %d = %d\n", a.denominator, b.denominator, a.denominator % b.denominator);
         if (a.denominator % b.denominator != 0) {
             fmCreateFloat(b.denominator, b.denominator, &a_mul);
             fmCreateFloat(a.denominator, a.denominator, &b_mul);
@@ -31,18 +33,20 @@ unsigned int fmFloatOpAdd(FMFloat a, FMFloat b, FMFloat* p_result) {
 
     fmFloatOpMultiply(a, a_mul, &a_r);
     fmFloatOpMultiply(b, b_mul, &b_r);
-    // printf("a_r.n(%d) + b_r.n(%d) = %d\n", a_r.numerator, b_r.numerator, a_r.numerator + b_r.numerator);
     p_result->numerator = a_r.numerator + b_r.numerator;
     p_result->denominator = a_r.denominator;
 
     return FM_ERR_OK;
 }
+
+/** 
+ ** Substracts FMFloat b from FMFloat a and writes at p_result
+*/
 unsigned int fmFloatOpSubstract(FMFloat a, FMFloat b, FMFloat* p_result) {
     FMFloat a_mul;
     FMFloat b_mul;
 
     if (a.denominator != b.denominator) {
-        // printf("%d dwr %d = %d\n", a.denominator, b.denominator, a.denominator % b.denominator);
         if (a.denominator % b.denominator != 0) {
             fmCreateFloat(b.denominator, b.denominator, &a_mul);
             fmCreateFloat(a.denominator, a.denominator, &b_mul);
@@ -65,49 +69,60 @@ unsigned int fmFloatOpSubstract(FMFloat a, FMFloat b, FMFloat* p_result) {
 
     fmFloatOpMultiply(a, a_mul, &a_r);
     fmFloatOpMultiply(b, b_mul, &b_r);
-    // printf("a_r.n(%d) + b_r.n(%d) = %d\n", a_r.numerator, b_r.numerator, a_r.numerator + b_r.numerator);
     p_result->numerator = a_r.numerator - b_r.numerator;
     p_result->denominator = a_r.denominator;
 
     return FM_ERR_OK;
 }
+/**
+ ** Multiplies FMFloat a with FMFloat b and writes at p_result
+*/
 unsigned int fmFloatOpMultiply(FMFloat a, FMFloat b, FMFloat* p_result) {
     p_result->numerator = a.numerator * b.numerator;
     p_result->denominator = a.denominator * b.denominator;
     return FM_ERR_OK;
 }
+/** 
+ ** Divides FMFloat a by FMFloat b and writes at p_result
+*/
 unsigned int fmFloatOpDivide(FMFloat a, FMFloat b, FMFloat* p_result) {
     p_result->numerator = a.numerator * b.denominator;
     p_result->denominator = a.denominator * b.numerator;
     return FM_ERR_OK;
 }
 
-unsigned int fmFloatOpToMixedNumber(FMFloat a, FMFloat* p_result_float, signed int* pdecimal) {
+/**
+ ** Returns right float in p_result_float and decimal number to pdecimal
+*/
+unsigned int fmFloatOpToMixedNumber(FMFloat a, FMFloat* presult, signed int* pdecimal) {
     if (a.numerator - a.numerator % a.denominator != 0) {
         *pdecimal = a.numerator / a.denominator;
     } else {
-        // printf("\npdecimal: %d", 0);
         *pdecimal = 0;
     }
-    p_result_float->numerator = a.numerator % a.denominator;
-    p_result_float->denominator = a.denominator;
+    presult->numerator = a.numerator % a.denominator;
+    presult->denominator = a.denominator;
     return FM_ERR_OK;
 }
-
-unsigned int fmFloatOpSimplify(FMFloat a, FMFloat* p_result_float) {
+/**
+ ** simplifies fraction number
+*/
+unsigned int fmFloatOpSimplify(FMFloat a, FMFloat* presult) {
     for (int i = min(a.numerator, a.denominator); i > 0; i--) {
         if (a.numerator % i == 0 && a.denominator % i == 0) {
-            p_result_float->numerator = a.numerator / i;
-            p_result_float->denominator = a.denominator / i;
+            presult->numerator = a.numerator / i;
+            presult->denominator = a.denominator / i;
             return FM_ERR_OK;
         }
     }
-    p_result_float->numerator = a.numerator / 1;
-    p_result_float->denominator = a.denominator / 1;
+    presult->numerator = a.numerator / 1;
+    presult->denominator = a.denominator / 1;
 
     return FM_ERR_CANT_SIMPLIFY;
 }
-
+/**
+ ** Prints float as numerator/denomirator, like fraction
+*/
 unsigned int fmFloatPrintf(FMFloat fmfloat) {
     printf("%d/%d", fmfloat.numerator, fmfloat.denominator);
 
