9d67d5ec12295d501a4398ea885fd24b6246b558
stalker57241 <stalker3204@yandex.com>
2022-12-31 20:05:22 +0300

    added build scripts for win32 and win64

---
+++ build_win64.bat
@@ -0,0 +1,2 @@
+cmake -H. -Bbuild/win/x64 -A x64 
+cmake --build build/win/x64 --target ALL_BUILD --config Release 
---
+++ build_win86.bat
@@ -0,0 +1,2 @@
+cmake -H. -Bbuild/win/x86 
+cmake --build build/win/x86 --target ALL_BUILD --config Release
